829a91b9369bdc290bccf7dc9d3793e0d76dc8e4
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-02-23 18:27:12 -0800

    Link to reg panel

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,9 @@
 class RegistrationsController < ApplicationController
   before_action :set_event
+  before_action :authenticate_user!
 
   def index
+    authorize @event.registrations
     @registrations = @event.registrations.decorate
   end
 
@@ -9,6 +11,7 @@ class RegistrationsController < ApplicationController
     @registration = @event.registrations.new(
       registration_params.merge!(user_id: current_user.id)
     )
+    authorize @registration
     if @registration.save
       redirect_to @event, notice: 'Registration successful. Please print your ticket.'
       RegistrationMailer.new_registration(@registration.id).deliver_now
@@ -19,11 +22,13 @@ class RegistrationsController < ApplicationController
 
   def ticket
     @registration = Registration.find(params[:id])
+    authorize @registration
     @event = @registration.event.decorate
   end
 
   def approve
     @registration = Registration.find params[:id]
+    authorize @registration
     if @registration.approve!
       redirect_to event_registrations_path(@event), notice: 'Registration approved!'
     else
---
+++ app/policies/registration_policy.rb
@@ -0,0 +1,23 @@
+class RegistrationPolicy < ApplicationPolicy
+  def index?
+    user.admin? || user.editor?
+  end
+
+  def create?
+    !(record.event..user == user)
+  end
+
+  def approve?
+    (record.event.user == user) || user.admin?
+  end
+
+  def ticket?
+    true
+  end
+
+  class Scope < Scope
+    def resolve
+      scope
+    end
+  end
+end
